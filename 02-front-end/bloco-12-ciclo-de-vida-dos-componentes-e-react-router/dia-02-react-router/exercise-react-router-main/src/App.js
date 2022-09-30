f73be30cfdaefe9f26e5811d830484a772f1de53
tulio <tulio.amorimm@gmail.com>
2022-09-30 19:26:13 -0300

    dd

--- 02-front-end/bloco-12-ciclo-de-vida-dos-componentes-e-react-router/dia-02-react-router/exercise-react-router-main/src/App.js
+++ 02-front-end/bloco-12-ciclo-de-vida-dos-componentes-e-react-router/dia-02-react-router/exercise-react-router-main/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <Home />
+      </BrowserRouter>
     );
   }
 }
